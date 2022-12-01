94bb375a108da9ee48ad1489cd55e8858df65f9b
Bryan Veneruso <bryanv@qualtrics.com>
2022-12-01 09:23:57 -0800

    Add timing to d1p2

--- days/day1/p2.js
+++ days/day1/p2.js
@@ -7,20 +7,22 @@ let currCount = 0;
 
 // Main async run function
 async function run() {
+	let startTime = Date.now();
 	await filereader.processLineByLine(__dirname + '/input2.txt', function(line) {
 		line.length === 0 ? updateMaxCount() : currCount += Number(line);
 	});
 	updateMaxCount();
 	console.log(sortedMaxes.reduce((sum, curr) => sum + curr));
+	console.log(`Time taken ${Date.now() - startTime}ms`);
 }
 
 // Helper function to reduce duplication
 function updateMaxCount() {
-	if(sortedMaxes.length < MAXES_TO_TRACK + 1) {
+	if(sortedMaxes.length < MAXES_TO_TRACK || currCount > sortedMaxes[0]) {
 		sortedMaxes.push(Number(currCount));
+		sortedMaxes.sort((a, b) => a - b)
 	}
 	if(sortedMaxes.length === MAXES_TO_TRACK + 1) {
-		sortedMaxes.sort((a, b) => a - b)
 		sortedMaxes.splice(0, 1);
 	}
 	currCount = 0;
